6878738ea312820d09d619a263b4b5b440133698
Guilherme-mendesc <guimendes1337@gmail.com>
2022-10-06 17:14:32 -0300

    Atualizacao model loja

--- src/main/java/com/crud/lojas/model/Lojas.java
+++ src/main/java/com/crud/lojas/model/Lojas.java
@@ -1,16 +1,15 @@
 package com.crud.lojas.model;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -31,89 +30,93 @@ public class Lojas {
 	private String nome;
 	
 	@NotNull
-	@Temporal(TemporalType.DATE)
-	private Date dataFundacao = new java.sql.Date(System.currentTimeMillis());
-	
-	@NotNull
 	@Size(min=3, max = 100)
 	private String cnpj;
 	
-	@ManyToOne
+	@NotNull
+	@Size(min=3, max=10)
+	private String dataFundacao;
+	
+	@OneToMany(mappedBy ="lojas" , cascade = CascadeType.ALL)
 	@JsonIgnoreProperties("lojas")
-	private Produto produtos;
+	private List<Produto> produtos;
 	
 
 	public Lojas() {}
 
-	public Lojas(long id, @NotNull @Size(min = 3, max = 100) String nome, @NotNull Date dataFundacao,
-			@NotNull @Size(min = 3, max = 100) String cnpj) {
-		super();
+
+	public Lojas(long id, @NotNull @Size(min = 3, max = 100) String nome,
+			@NotNull @Size(min = 3, max = 100) String cnpj, @NotNull @Size(min = 3, max = 10) String dataFundacao,
+			List<Produto> produtos) {
 		this.id = id;
 		this.nome = nome;
-		this.dataFundacao = dataFundacao;
 		this.cnpj = cnpj;
+		this.dataFundacao = dataFundacao;
+		this.produtos = produtos;
 	}
+
 
 	public long getId() {
 		return id;
 	}
 
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 
 	public String getNome() {
 		return nome;
 	}
 
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
-	public Date getdataFundacao() {
-		return dataFundacao;
-	}
-
-	public void setData(Date data) {
-		this.dataFundacao = data;
-	}
 
 	public String getCnpj() {
 		return cnpj;
 	}
 
+
 	public void setCnpj(String cnpj) {
 		this.cnpj = cnpj;
 	}
-	
-	public Date getDataFundacao() {
+
+
+	public String getDataFundacao() {
 		return dataFundacao;
 	}
 
-	public void setDataFundacao(Date dataFundacao) {
+
+	public void setDataFundacao(String dataFundacao) {
 		this.dataFundacao = dataFundacao;
 	}
 
-	public Produto getProdutos() {
+
+	public List<Produto> getProdutos() {
 		return produtos;
 	}
 
-	public void setProdutos(Produto produtos) {
+
+	public void setProdutos(List<Produto> produtos) {
 		this.produtos = produtos;
 	}
 
+
 	@Override
 	public String toString() {
-		return "Lojas [id=" + id + ", nome=" + nome + ", data=" + dataFundacao + ", cnpj=" + cnpj + "]";
+		return "Lojas [id=" + id + ", nome=" + nome + ", cnpj=" + cnpj + ", dataFundacao=" + dataFundacao
+				+ ", produtos=" + produtos + "]";
 	}
+	
+
+	
 
 	
 	
-	
-	
-	
-	
-
 	}
 	
 	
--- src/main/java/com/crud/lojas/model/Produto.java
+++ src/main/java/com/crud/lojas/model/Produto.java
@@ -2,12 +2,11 @@ package com.crud.lojas.model;
 
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -22,13 +21,69 @@ public class Produto {
 	private long id;
 	
 	@NotNull
-	private String nomeProdutos;
+	private String nomeProduto;
 	
 	@NotNull
 	private Double preco;
 	
-	@OneToMany(mappedBy ="lojas" , cascade = CascadeType.ALL)
-	@JsonIgnoreProperties("lojas")
-	private List<Lojas> lojas;
+	@ManyToOne
+	@JsonIgnoreProperties("produtos")
+	private Lojas lojas;
+
+	private Produto() {
+		
+	}	
+	
+	public Produto(long id, String nomeProduto, Double preco, Lojas lojas) {
+		
+		this.id = id;
+		this.nomeProduto = nomeProduto;
+		this.preco = preco;
+		this.lojas = lojas;
+	}
+
+
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Lojas getLojas() {
+		return lojas;
+	}
+
+	public void setLojas(Lojas lojas) {
+		this.lojas = lojas;
+	}
+
+	@Override
+	public String toString() {
+		return "Produto [id=" + id + ", nomeProduto=" + nomeProduto + ", preco=" + preco + ", lojas=" + lojas + "]";
+	}
+	
+	
+	
 	
 }
+
+
